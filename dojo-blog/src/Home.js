16a9d9c936fc3078ad13f70312d0d82db8cd1de7
Hashem Gray <Hgray1109@gmail.com>
2021-09-29 08:33:59 -0400

    Youtube Tutorial L8

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,27 @@
+import { useState } from 'react'
+
+
 const Home = () => {
+    // let name = 'mario';
+    // const handleClick = () => {
+      const [name, setname]= useState('mario');
+
+    const handleClick = () => {
+        setname('Luigi')
+    }
+
+    // const handleClickAgain = (name) => {
+    //     console.log('Hello ' + name);
+
+    // }
+
     return (
         <div className ="home">
         <h2> Homepage </h2>
+        <p> { name } </p>
+        <button onClick={handleClick}>Click Me</button>
+        {/* <button onClick={() =>
+        handleClickAgain('Mario')}> Click me Agagin</button> */}
         </div>
     );
 }
