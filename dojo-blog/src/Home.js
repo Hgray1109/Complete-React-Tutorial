39a70b7c03c5c4011e9f5bd9f9ae2d111195a6d7
Hashem Gray <Hgray1109@gmail.com>
2021-09-29 09:18:55 -0400

    Youtube Tutorial L10

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,30 +1,73 @@
 import { useState } from 'react'
 
 
-const Home = () => {
-    // let name = 'mario';
-    // const handleClick = () => {
-      const [name, setname]= useState('mario');
+const Home = () => { 
+    const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
 
-    const handleClick = () => {
-        setname('Luigi')
-    }
+return (
+    <div className ="home">
+        {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2> {blog.title}</h2>
+                <p> Written by {blog.author} </p>
 
-    // const handleClickAgain = (name) => {
-    //     console.log('Hello ' + name);
 
-    // }
+            </div>
+        ))}
 
-    return (
-        <div className ="home">
-        <h2> Homepage </h2>
-        <p> { name } </p>
-        <button onClick={handleClick}>Click Me</button>
-        {/* <button onClick={() =>
-        handleClickAgain('Mario')}> Click me Agagin</button> */}
         </div>
-    );
+        )
 }
+        
+        
+        export default Home;
+        
+        
+        
+        
+        // const handleClickAgain = (name) => {
+            //     console.log('Hello ' + name);
+            
+            // }
+            
+        
+        
+        
+        
+        
+        
+        
+        // let name = 'mario';
+        // const handleClick = () => {
+            
+            
+            
+        // const [name, setName]= useState('mario');
+        // const [age, setAge] = useState(25);
+        
+        // const handleClick = () => {
+            //   setName('Luigi')
+            //   setAge(30)
+            
+            // );
+            
 
 
-export default Home;
+
+
+
+
+
+
+
+
+
+{/* <h2> Homepage </h2>
+    <p> Hi I'm { name } I am {age} years old!</p>
+<button onClick={handleClick}>Click Me</button> */}
+{/* <button onClick={() =>
+handleClickAgain('Mario')}> Click me Agagin</button> */}
