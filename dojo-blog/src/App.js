96f33c8f654477b093aa5347b28038e41eb5b38d
Hashem Gray <Hgray1109@gmail.com>
2021-09-29 00:53:48 -0400

    Youtube Tutorial L5

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  // const title = "Welcome to the new blog";
+  // const likes = 50;
+  // const person =  {name: 'yoshi', age: 30};
+
   return (
-    <div className="App">
+    <div className="App"> 
+    <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <Home />
+        {/* <h1>{ title }</h1> 
+        <p> Liked {likes} times </p> */}
+        {/* <p> {person} </p> */}
       </div>
     </div>
   );
